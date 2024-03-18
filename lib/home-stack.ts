52d641d9ef89a1dcf03ea86d90d084dc573db241
Derek Cross <derekcrosslu@gmail.com>
2024-03-18 18:04:04 -0500

    adding lambda test functions

--- lib/home-stack.ts
+++ lib/home-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HomeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a DynamoDB table
+    const table = new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'myPrimaryKey', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'mySortKey', type: dynamodb.AttributeType.NUMBER },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HomeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      versioned: true,
+    });
+
+    // Create a Lambda function
+    const lambdaFunction = new lambda.Function(this, 'MyLambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('./lambda/'),
+    });
+
+    // Grant read/write permissions to the Lambda function for the DynamoDB table
+    table.grantReadWriteData(lambdaFunction);
+
+    // Grant read/write permissions to the Lambda function for the S3 bucket
+    bucket.grantReadWrite(lambdaFunction);
   }
 }
--- test/home.test.ts
+++ test/home.test.ts
@@ -4,14 +4,56 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/home-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Home.HomeStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+// test('SQS Queue Created', () => {
+// //   const app = new cdk.App();
+// //     // WHEN
+// //   const stack = new Home.HomeStack(app, 'MyTestStack');
+// //     // THEN
+// //   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+// //   template.hasResourceProperties('AWS::SQS::Queue', {
+// //     VisibilityTimeout: 300
+// //   });
+// });
+
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { HomeStack } from '../lib/home-stack';
+
+describe('HomeStack', () => {
+  test('Stack contains a DynamoDB table', () => {
+    const app = new cdk.App();
+    const stack = new HomeStack(app, 'TestStack');
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+      KeySchema: [
+        { AttributeName: 'myPrimaryKey', KeyType: 'HASH' },
+        { AttributeName: 'mySortKey', KeyType: 'RANGE' },
+      ],
+    });
+  });
+
+  test('Stack contains an S3 bucket with versioning enabled', () => {
+    const app = new cdk.App();
+    const stack = new HomeStack(app, 'TestStack');
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      VersioningConfiguration: {
+        Status: 'Enabled',
+      },
+    });
+  });
+
+  test('Stack contains a Lambda function', () => {
+    const app = new cdk.App();
+    const stack = new HomeStack(app, 'TestStack');
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Handler: 'index.handler',
+      Runtime: 'nodejs18.x',
+    });
+  });
 });
